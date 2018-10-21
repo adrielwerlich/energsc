95431c730f7253a7861a051dba3b1ea5a248e7ae
adriel <adrielwerlich@gmail.com>
2018-10-21 14:17:54 -0300

    comm

---
+++ siteenergiasc/static/js/index.js
@@ -0,0 +1,11 @@
+// $('.carousel').slick({
+//   dots: true,
+//   infinite: true,
+//   speed: 500,
+//   slidesToShow: 4,
+//   slidesToScroll: 4,
+// });
+
+$('.carousel').carousel({
+    interval: 2000
+})
